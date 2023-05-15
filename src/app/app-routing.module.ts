92021aadfc579769d0a261801d681e0cdbff3fed
thanhtra17 <ttra170302@gmail.com>
2023-05-15 17:25:30 +0700

    router category + products, giao dien login+ regis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { CategoryComponent } from './page/category/category.component';
 import { HomeComponent } from './page/home/home.component';
 import { ProductsComponent } from './page/products/products.component';
@@ -7,7 +9,10 @@ import { ProductsComponent } from './page/products/products.component';
 const routes: Routes = [
   {path:``,component:HomeComponent},
   {path: `category`, component:CategoryComponent},
-  {path:`products`, component: ProductsComponent }
+  {path:`products`, component: ProductsComponent },
+  {path:`login`, component:LoginComponent},
+  {path:`register`, component:RegisterComponent}
+
 ];
 
 
