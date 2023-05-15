270e22ed0fe83838817c3c3f92cda21b2037b0d8
thanhtra17 <ttra170302@gmail.com>
2023-05-15 17:06:06 +0700

    router category + products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './page/category/category.component';
+import { HomeComponent } from './page/home/home.component';
+import { ProductsComponent } from './page/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:``,component:HomeComponent},
+  {path: `category`, component:CategoryComponent},
+  {path:`products`, component: ProductsComponent }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
