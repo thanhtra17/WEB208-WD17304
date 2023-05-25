c07c70f8ecd48830ed3617ef304ae6302016904b
thanhtra17 <ttra170302@gmail.com>
2023-05-25 23:11:46 +0700

    giao dien

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -19,7 +19,7 @@ export class HomeComponent {
       price: 100,
       description: 'Product 1 description',
       imgUrl:
-        'https://m.media-amazon.com/images/M/MV5BYzQ0ZWIxZjAtYWI3Yy00MGM0LWFjOGYtNzcyYThiOTA3ODI1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
+        'https://heliosjewels.vn/cdn/shop/files/1-2_1_460x.jpg?v=1682916204',
         isDescriptionVisible: false,
         
     },
@@ -29,7 +29,7 @@ export class HomeComponent {
       price: 200,
       description: 'Product 1 description',
       imgUrl:
-        'https://m.media-amazon.com/images/M/MV5BYzQ0ZWIxZjAtYWI3Yy00MGM0LWFjOGYtNzcyYThiOTA3ODI1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
+        'https://heliosjewels.vn/cdn/shop/products/1_14_460x.jpg?v=1681911366',
         isDescriptionVisible: false,
     },
     {
@@ -38,9 +38,18 @@ export class HomeComponent {
       price: 300,
       description: 'Product 1 description',
       imgUrl:
-        'https://m.media-amazon.com/images/M/MV5BYzQ0ZWIxZjAtYWI3Yy00MGM0LWFjOGYtNzcyYThiOTA3ODI1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg',
+        'https://heliosjewels.vn/cdn/shop/files/ELEGANTONYXRING_1_460x.jpg?v=1682916204',
         isDescriptionVisible: false,
     },
+    // {
+    //   id: '4',
+    //   name: 'Product 4',
+    //   price: 300,
+    //   description: 'Product 1 description',
+    //   imgUrl:
+    //     'https://heliosjewels.vn/cdn/shop/products/9_Custom_1_460x.jpg?v=1679989335',
+    //     isDescriptionVisible: false,
+    // },
   ];
   }
  
